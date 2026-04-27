5d8504b49f166d115a6406f88e356608c0d19089
diaskarina <karina.dias@sptech.school>
2026-04-27 20:14:45 -0300

    Cadastro e Login quase finalizado

--- Site/src/controllers/usuarioController.js
+++ Site/src/controllers/usuarioController.js
@@ -32,7 +32,7 @@ function autenticar(req, res) {
                                         aquarios: resultadoAquarios
                                     });
                                 } else {
-                                    res.status(204).json({ aquarios: [] });
+                                    res.status(200).json({ aquarios: [] });
                                 }
                             })
                     } else if (resultadoAutenticar.length == 0) {
@@ -58,22 +58,20 @@ function cadastrar(req, res) {
     var cpf = req.body.cpfServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu CPF a vincular está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
-    } else if (cpf == undefined)  {
-         res.status(400).send("Seu CPF a vincular está undefined!");
-    }else{
+
+    } else {
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa,cpf)
+        usuarioModel.cadastrar(nome, cpf, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
